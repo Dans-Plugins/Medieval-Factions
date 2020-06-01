1ccd90ee42738ff91bf804ddb8a1f8970fb1e664
dmccoystephenson <dmccoystephenson@gmail.com>
2020-05-31 18:19:19 -0600

    fixed everything, now the plugin works

--- src/plugin/Main.java
+++ src/plugin/Main.java
@@ -6,7 +6,11 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        System.out.println("Medieval Factions plugin enabling....");
 
+        // starting stuff
+
+        System.out.println("Medieval Factions plugin enabled.");
     }
 
     @Override
