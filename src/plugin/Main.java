7f69fdcb11f3c1a64a0809cc66f99e436fbd6c22
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-01 03:10:45 -0600

    added save and a test for it

--- src/plugin/Main.java
+++ src/plugin/Main.java
@@ -4,17 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+    Faction myFaction = new Faction("Alexandria");
+
     @Override
     public void onEnable() {
         System.out.println("Medieval Factions plugin enabling....");
 
-        // starting stuff
+        myFaction.changeName("Leyton");
 
         System.out.println("Medieval Factions plugin enabled.");
     }
 
     @Override
     public void onDisable(){
-
+        myFaction.save();
     }
 }
