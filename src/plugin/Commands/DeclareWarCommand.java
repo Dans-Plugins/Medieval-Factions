4310b687291678c154e5de14a8abd5746893b2c7
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-05 00:42:02 -0600

    added some messages

--- src/plugin/Commands/DeclareWarCommand.java
+++ src/plugin/Commands/DeclareWarCommand.java
@@ -1,5 +1,6 @@
 package plugin.Commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import plugin.Faction;
@@ -12,11 +13,11 @@ public class DeclareWarCommand {
     public static void declareWar(CommandSender sender, String[] args, ArrayList<Faction> factions) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
+            boolean owner = false;
             for (Faction faction : factions) {
-
                 // if player is the owner
                 if (faction.isOwner(player.getName())) {
-
+                    owner = true;
                     // if there's more than one argument
                     if (args.length > 1) {
 
@@ -29,12 +30,18 @@ public class DeclareWarCommand {
 
                                 // add enemy
                                 faction.addEnemy(factionName);
-
+                                player.sendMessage(ChatColor.AQUA + "War has been declared against " + factionName + "!");
                             }
                         }
 
                     }
+                    else {
+                        player.sendMessage(ChatColor.RED + "Usage: /mf declarewar (faction-name)");
+                    }
                 }
+            }
+            if (!owner) {
+                player.sendMessage(ChatColor.RED + "You have to own a faction to use this command.");
             }
         }
     }
