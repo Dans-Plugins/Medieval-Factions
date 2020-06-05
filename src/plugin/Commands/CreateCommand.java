7f171b8004bc5ea4684d9c8db4af6435350fe62e
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-05 03:22:05 -0600

    fixed creation duplication wrongful check

--- src/plugin/Commands/CreateCommand.java
+++ src/plugin/Commands/CreateCommand.java
@@ -27,10 +27,13 @@ public class CreateCommand {
             // argument check
             if (args.length > 1) {
 
+                // creating name from arguments 1 to the last one
+                String name = createStringFromFirstArgOnwards(args);
+
                 // faction existence check
                 boolean factionExists = false;
                 for (Faction faction : factions) {
-                    if (faction.getName().equalsIgnoreCase(args[1])) {
+                    if (faction.getName().equalsIgnoreCase(name)) {
                         factionExists = true;
                         break;
                     }
@@ -38,14 +41,11 @@ public class CreateCommand {
 
                 if (!factionExists) {
 
-                    // creating name from arguments 1 to the last one
-                    String name = createStringFromFirstArgOnwards(args);
-
                     // actual faction creation
                     Faction temp = new Faction(name, player.getName());
                     factions.add(temp);
                     factions.get(factions.size() - 1).addMember(player.getName());
-                    System.out.println("Faction " + args[1] + " created.");
+                    System.out.println("Faction " + name + " created.");
                     player.sendMessage(ChatColor.AQUA + "Faction " + name + " created.");
                     return true;
                 }
