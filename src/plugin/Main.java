058cb2ad5ecfdbe20daf0c37e232b39df0652c12
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-04 21:27:06 -0600

    cleaned up /mf help

--- src/plugin/Main.java
+++ src/plugin/Main.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import plugin.Commands.HelpCommand;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -112,24 +113,7 @@ public class Main extends JavaPlugin {
 
                 // help command
                 if (args[0].equalsIgnoreCase("help")) {
-                    sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + "Medieval Factions Commands" + "\n----------\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf help - Show list of useful commands." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf create - Create a new faction." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf list - List all factions on the server." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf delete - Delete your faction (must be owner)." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf members - List the members of your faction." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf info - See your faction information." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf desc - Set your faction description." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf invite - Invite a player to your faction." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf join - Join a faction if you've been invited." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf kick - Kick a player from your faction (must be owner). " + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf leave - Leave your current faction." + "\n");
-                    sender.sendMessage(ChatColor.AQUA + "/mf transfer - Transfer ownership of your faction to another player (must be owner).\n");
-                    if (!(sender instanceof Player)) {
-                        sender.sendMessage(ChatColor.AQUA + "/mf forcesave - Force the plugin to save.");
-                        sender.sendMessage(ChatColor.AQUA + "/mf forceload - Force the plugin to load.");
-                    }
-                    sender.sendMessage(ChatColor.AQUA + "----------\n");
+                    HelpCommand.sendHelpMessage(sender);
                 }
 
                 // create command
