e725b9fb72c9dfa2c065aa1065bbe15f6dd5b837
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-05 01:08:10 -0600

    got rid of an unused import

--- src/plugin/Main.java
+++ src/plugin/Main.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.Commands.*;
 
