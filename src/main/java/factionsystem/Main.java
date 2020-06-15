fd845974826e89b9806e32e08a1c03763ea9f821
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-14 22:43:03 -0600

    refactored kick command

--- src/main/java/factionsystem/Main.java
+++ src/main/java/factionsystem/Main.java
@@ -377,7 +377,8 @@ public class Main extends JavaPlugin implements Listener {
                 // kick command
                 if (args[0].equalsIgnoreCase("kick")) {
                     if (sender.hasPermission("mf.kick") || sender.hasPermission("mf.default")) {
-                        KickCommand.kickPlayer(sender, args, factions, playerPowerRecords);
+                        KickCommand command = new KickCommand(this);
+                        command.kickPlayer(sender, args);
                     }
                     else {
                         sender.sendMessage(ChatColor.RED + "Sorry! You need the following permission to use this command: 'mf.kick'");
