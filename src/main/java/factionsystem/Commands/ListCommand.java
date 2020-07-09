b8b9d708c5f22b275f5f2b84bc11c9236215b0ba
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-09 16:00:02 -0600

    spacing tweaks, good god

--- src/main/java/factionsystem/Commands/ListCommand.java
+++ src/main/java/factionsystem/Commands/ListCommand.java
@@ -39,7 +39,7 @@ public class ListCommand {
         int longestNameLength = main.utilities.getLongestStringLength(factionNames);
 
         String headers = "";
-        headers = headers + "Name";
+        headers = headers + "Name" + " ";
         for (int i = 0; i < longestNameLength - 4; i++) {
             headers = headers + " ";
         }
@@ -47,7 +47,7 @@ public class ListCommand {
 
         sender.sendMessage(ChatColor.AQUA + headers);
         for (Faction faction : main.utilities.getFactionsSortedByPower()) {
-            sender.sendMessage(ChatColor.AQUA + "" + faction.getName() + "  " + faction.getCumulativePowerLevel() + "      " + faction.getPopulation() + "           " + getChunksClaimedByFaction(faction.getName(), main.claimedChunks));
+            sender.sendMessage(ChatColor.AQUA + "" + faction.getName() + " " + faction.getCumulativePowerLevel() + "      " + faction.getPopulation() + "           " + getChunksClaimedByFaction(faction.getName(), main.claimedChunks));
         }
     }
 }
