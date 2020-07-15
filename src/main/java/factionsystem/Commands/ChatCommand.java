c70331a2e85396b795d7c830838f212018649640
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-14 23:32:46 -0600

    added a faction check

--- src/main/java/factionsystem/Commands/ChatCommand.java
+++ src/main/java/factionsystem/Commands/ChatCommand.java
@@ -5,6 +5,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static factionsystem.Subsystems.UtilitySubsystem.isInFaction;
+
 public class ChatCommand {
 
     Main main = null;
@@ -18,14 +20,20 @@ public class ChatCommand {
             Player player = (Player) sender;
 
             if (player.hasPermission("mf.chat") || player.hasPermission("mf.default")) {
-                if (!main.playersInFactionChat.contains(player.getName())) {
-                    main.playersInFactionChat.add(player.getName());
-                    player.sendMessage(ChatColor.GREEN + "You are now speaking in faction chat!");
+                if (isInFaction(player.getName(), main.factions)) {
+                    if (!main.playersInFactionChat.contains(player.getName())) {
+                        main.playersInFactionChat.add(player.getName());
+                        player.sendMessage(ChatColor.GREEN + "You are now speaking in faction chat!");
+                    }
+                    else {
+                        main.playersInFactionChat.remove(player.getName());
+                        player.sendMessage(ChatColor.GREEN + "You are no longer in faction chat!");
+                    }
                 }
                 else {
-                    main.playersInFactionChat.remove(player.getName());
-                    player.sendMessage(ChatColor.GREEN + "You are no longer in faction chat!");
+                    player.sendMessage(ChatColor.RED + "You must be in a faction to use this command!");
                 }
+
             }
             else {
                 player.sendMessage(ChatColor.RED + "Sorry! In order to use this command, you need the following permission: 'mf.chat'");
