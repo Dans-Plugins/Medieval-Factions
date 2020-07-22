84dff31d757df7276ffb53bdddf37956807b83a2
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-21 20:08:59 -0600

    removed unused import

--- src/main/java/factionsystem/Main.java
+++ src/main/java/factionsystem/Main.java
@@ -11,7 +11,6 @@ import factionsystem.Subsystems.UtilitySubsystem;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
