2ee142252866a2ae121cdc9769d787fee6fcb5f9
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-25 03:09:46 -0600

    removed a message

--- src/main/java/factionsystem/EventHandlers/PlayerInteractEventHandler.java
+++ src/main/java/factionsystem/EventHandlers/PlayerInteractEventHandler.java
@@ -255,7 +255,6 @@ public class PlayerInteractEventHandler {
                             return;
                         }
                     }
-                    System.out.println("cancelled!");
                     event.setCancelled(true);
                     return;
                 }
