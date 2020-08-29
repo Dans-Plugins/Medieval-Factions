a77b80289ded795646ca6bf547b2ae286506108f
dmccoystephenson <dmccoystephenson@gmail.com>
2020-08-29 00:56:57 -0600

    players should now be informed upon login if their faction is exceeding their demesne limit

--- src/main/java/factionsystem/EventHandlers/PlayerJoinEventHandler.java
+++ src/main/java/factionsystem/EventHandlers/PlayerJoinEventHandler.java
@@ -1,8 +1,14 @@
 package factionsystem.EventHandlers;
 
 import factionsystem.Main;
+import factionsystem.Objects.Faction;
 import factionsystem.Objects.PlayerPowerRecord;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import static factionsystem.Subsystems.UtilitySubsystem.getChunksClaimedByFaction;
+import static factionsystem.Subsystems.UtilitySubsystem.getPlayersFaction;
 
 public class PlayerJoinEventHandler {
 
@@ -20,5 +26,20 @@ public class PlayerJoinEventHandler {
                     main);
             main.playerPowerRecords.add(newRecord);
         }
+
+        informPlayerIfTheirLandIsInDanger(event.getPlayer());
+    }
+
+    public void informPlayerIfTheirLandIsInDanger(Player player) {
+        Faction faction = getPlayersFaction(player.getUniqueId(), main.factions);
+        if (faction != null) {
+            if (isFactionExceedingTheirDemesneLimit(faction)) {
+                player.sendMessage(ChatColor.RED + "Your faction has more claimed chunks than power! Your land can be conquered!");
+            }
+        }
+    }
+
+    public boolean isFactionExceedingTheirDemesneLimit(Faction faction) {
+        return (getChunksClaimedByFaction(faction.getName(), main.claimedChunks) > faction.getCumulativePowerLevel());
     }
 }
