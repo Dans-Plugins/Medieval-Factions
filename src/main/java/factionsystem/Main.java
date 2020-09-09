cbb75b3d0fd945fcf6c6425b90b93b249d80bb18
dmccoystephenson <dmccoystephenson@gmail.com>
2020-09-09 15:07:28 -0600

    a few tweaks

--- src/main/java/factionsystem/Main.java
+++ src/main/java/factionsystem/Main.java
@@ -107,8 +107,6 @@ public class Main extends JavaPlugin implements Listener {
         return commandInterpreter.interpretCommand(sender, label, args);
     }
 
-    // event handlers
-
     @EventHandler()
     public void onDamage(EntityDamageByEntityEvent event) {
         EntityDamageByEntityEventHandler handler = new EntityDamageByEntityEventHandler(this);
@@ -126,6 +124,7 @@ public class Main extends JavaPlugin implements Listener {
         BlockBreakEventHandler handler = new BlockBreakEventHandler(this);
         handler.handle(event);
     }
+
     @EventHandler()
     public void onBlockPlace(BlockPlaceEvent event) {
         BlockPlaceEventHandler handler = new BlockPlaceEventHandler(this);
