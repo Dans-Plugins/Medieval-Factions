f338e1e1e3271dab3a691aa0b2991775f9d8ae3e
Phil Garner <phil.b.garner@gmail.com>
2020-09-14 21:07:28 -0400

    I think this fixes it, both /mf forcesave and onDisable call the storage.save() method, so adding the saveConfig() bukkit statement did the trick.

--- src/main/java/factionsystem/Subsystems/StorageSubsystem.java
+++ src/main/java/factionsystem/Subsystems/StorageSubsystem.java
@@ -44,6 +44,7 @@ public class StorageSubsystem {
         savePlayerPowerRecords();
         savePlayerActivityRecords();
         saveLockedBlocks();
+        main.saveConfig();
     }
 
     private void saveFactions() {
