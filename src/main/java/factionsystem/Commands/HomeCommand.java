065608f08002d9841b136ed2e6e61f813b948aac
dmccoystephenson <dmccoystephenson@gmail.com>
2020-11-28 20:15:06 -0700

    refactored more commands

--- src/main/java/factionsystem/Commands/HomeCommand.java
+++ src/main/java/factionsystem/Commands/HomeCommand.java
@@ -21,57 +21,63 @@ public class HomeCommand extends Command {
     public void teleportPlayer(CommandSender sender) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (isInFaction(player.getUniqueId(), main.factions)) {
-                Faction playersFaction = getPlayersFaction(player.getUniqueId(), main.factions);
-                if (playersFaction.getFactionHome() != null) {
 
-                    // Check that factionHome is in it's own factions land and not claimed by someone else.
-                    Chunk homeChunk = playersFaction.getFactionHome().getBlock().getChunk();
-                    if (isClaimed(homeChunk, main.claimedChunks)){
-                        // Ensure is in your faction
-                        ClaimedChunk claimedHomeChunk = getClaimedChunk(homeChunk.getX(), homeChunk.getZ(), homeChunk.getWorld().getName(), main.claimedChunks);
-                        if (claimedHomeChunk.getHolder() != null && !playersFaction.getName().equals(claimedHomeChunk.getHolder())) {
-                            // Area is claimed by someone else and cannot be home. Cancel teleport and return;
-                            player.sendMessage(ChatColor.RED + "Home was claimed by another faction, and has been lost.");
+            if (sender.hasPermission("mf.home") || sender.hasPermission("mf.default")) {
+                if (isInFaction(player.getUniqueId(), main.factions)) {
+                    Faction playersFaction = getPlayersFaction(player.getUniqueId(), main.factions);
+                    if (playersFaction.getFactionHome() != null) {
+
+                        // Check that factionHome is in it's own factions land and not claimed by someone else.
+                        Chunk homeChunk = playersFaction.getFactionHome().getBlock().getChunk();
+                        if (isClaimed(homeChunk, main.claimedChunks)){
+                            // Ensure is in your faction
+                            ClaimedChunk claimedHomeChunk = getClaimedChunk(homeChunk.getX(), homeChunk.getZ(), homeChunk.getWorld().getName(), main.claimedChunks);
+                            if (claimedHomeChunk.getHolder() != null && !playersFaction.getName().equals(claimedHomeChunk.getHolder())) {
+                                // Area is claimed by someone else and cannot be home. Cancel teleport and return;
+                                player.sendMessage(ChatColor.RED + "Home was claimed by another faction, and has been lost.");
+                                return;
+                            }
+                        } else {
+                            // Area isn't claimed cannot be home. Cancel teleport and return;
+                            player.sendMessage(ChatColor.RED + "Home is in an unclaimed chunk, and is unavailable");
                             return;
                         }
-                    } else {
-                        // Area isn't claimed cannot be home. Cancel teleport and return;
-                        player.sendMessage(ChatColor.RED + "Home is in an unclaimed chunk, and is unavailable");
-                        return;
+
+
+                        player.sendMessage(ChatColor.GREEN + "Teleporting in 3 seconds...");
+                        int seconds = 3;
+
+                        Location initialLocation = player.getLocation();
+
+                        getServer().getScheduler().runTaskLater(main, new Runnable() {
+                            @Override
+                            public void run() {
+                                if (initialLocation.getX() == player.getLocation().getX() &&
+                                        initialLocation.getY() == player.getLocation().getY() &&
+                                        initialLocation.getZ() == player.getLocation().getZ()) {
+
+                                    // teleport the player
+                                    player.teleport(playersFaction.getFactionHome());
+
+                                }
+                                else {
+                                    player.sendMessage(ChatColor.RED + "Movement Detected. Teleport cancelled.");
+                                }
+
+                            }
+                        }, seconds * 20);
+
                     }
-
-
-                    player.sendMessage(ChatColor.GREEN + "Teleporting in 3 seconds...");
-                    int seconds = 3;
-
-                    Location initialLocation = player.getLocation();
-
-                    getServer().getScheduler().runTaskLater(main, new Runnable() {
-                        @Override
-                        public void run() {
-                            if (initialLocation.getX() == player.getLocation().getX() &&
-                                initialLocation.getY() == player.getLocation().getY() &&
-                                initialLocation.getZ() == player.getLocation().getZ()) {
-
-                                // teleport the player
-                                player.teleport(playersFaction.getFactionHome());
-
-                            }
-                            else {
-                                player.sendMessage(ChatColor.RED + "Movement Detected. Teleport cancelled.");
-                            }
-
-                        }
-                    }, seconds * 20);
-
+                    else {
+                        player.sendMessage(ChatColor.RED + "The faction home isn't set yet.");
+                    }
                 }
                 else {
-                    player.sendMessage(ChatColor.RED + "The faction home isn't set yet.");
+                    player.sendMessage(ChatColor.RED + "You need to be in a faction to use this command.");
                 }
             }
             else {
-                player.sendMessage(ChatColor.RED + "You need to be in a faction to use this command.");
+                sender.sendMessage(ChatColor.RED + "Sorry! You need the following permission to use this command: 'mf.home'");
             }
         }
     }
--- src/main/java/factionsystem/Subsystems/CommandSubsystem.java
+++ src/main/java/factionsystem/Subsystems/CommandSubsystem.java
@@ -441,15 +441,9 @@ public class CommandSubsystem {
 
                 // home command
                 if (args[0].equalsIgnoreCase("home")) {
-                    if (sender.hasPermission("mf.home") || sender.hasPermission("mf.default")) {
-                        HomeCommand command = new HomeCommand(main);
-                        command.teleportPlayer(sender);
-                        return true;
-                    }
-                    else {
-                        sender.sendMessage(ChatColor.RED + "Sorry! You need the following permission to use this command: 'mf.home'");
-                        return false;
-                    }
+                    HomeCommand command = new HomeCommand(main);
+                    command.teleportPlayer(sender);
+                    return true;
                 }
 
                 // version command
