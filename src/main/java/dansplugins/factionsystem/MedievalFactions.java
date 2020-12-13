a4aeb340d105fc97b630af306bc3894ff4f6c9a8
dmccoystephenson <dmccoystephenson@gmail.com>
2020-12-12 23:26:10 -0700

    removed a couple comments

--- src/main/java/dansplugins/factionsystem/MedievalFactions.java
+++ src/main/java/dansplugins/factionsystem/MedievalFactions.java
@@ -10,10 +10,8 @@ import java.io.File;
 
 public class MedievalFactions extends JavaPlugin {
 
-    // instance
     private static MedievalFactions instance;
 
-    // version
     private String version = "v3.6.0.3-beta-9";
 
     public static MedievalFactions getInstance() {
