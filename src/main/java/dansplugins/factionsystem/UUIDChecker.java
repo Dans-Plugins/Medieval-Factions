bb6d40fe919430f3cffcc2912ca6c450fa2a3dd0
dmccoystephenson <dmccoystephenson@gmail.com>
2020-12-16 14:05:54 -0700

    made findUUIDBasedOnPlayerName ignore case when comparing player names

--- src/main/java/dansplugins/factionsystem/UUIDChecker.java
+++ src/main/java/dansplugins/factionsystem/UUIDChecker.java
@@ -44,7 +44,7 @@ public class UUIDChecker {
     public UUID findUUIDBasedOnPlayerName(String playerName){
         // Check online
         for (Player player : getOnlinePlayers()){
-            if (player.getName().equals(playerName)){
+            if (player.getName().equalsIgnoreCase(playerName)){
                 return player.getUniqueId();
             }
         }
@@ -52,7 +52,7 @@ public class UUIDChecker {
         // Check offline
         for (OfflinePlayer player : getOfflinePlayers()){
             try {
-                if (player.getName().equals(playerName)){
+                if (player.getName().equalsIgnoreCase(playerName)){
                     return player.getUniqueId();
                 }
             } catch (NullPointerException e) {
