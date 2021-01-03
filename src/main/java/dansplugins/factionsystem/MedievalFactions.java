698f2613cd8c6ee02b041a6d9bcb85d550f1ba99
dmccoystephenson <dmccoystephenson@gmail.com>
2021-01-02 19:17:33 -0700

    incremented version to beta 6

--- src/main/java/dansplugins/factionsystem/MedievalFactions.java
+++ src/main/java/dansplugins/factionsystem/MedievalFactions.java
@@ -16,7 +16,7 @@ public class MedievalFactions extends JavaPlugin {
 
     private static MedievalFactions instance;
 
-    private String version = "v3.7-beta-5";
+    private String version = "v3.7-beta-6";
 
     public static MedievalFactions getInstance() {
         return instance;
