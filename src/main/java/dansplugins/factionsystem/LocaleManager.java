8f0db9d6eb426ff47f4cd87ef7930016e65455d0
dmccoystephenson <dmccoystephenson@gmail.com>
2021-01-07 23:49:25 -0700

    implemented loadFromJSON()

--- src/main/java/dansplugins/factionsystem/LocaleManager.java
+++ src/main/java/dansplugins/factionsystem/LocaleManager.java
@@ -1,6 +1,11 @@
 package dansplugins.factionsystem;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
 import java.util.HashMap;
+import java.util.Set;
 
 public class LocaleManager {
 
@@ -32,7 +37,22 @@ public class LocaleManager {
     }
 
     private void loadFromJSON() {
-        // TODO
+        JSONParser parser = new JSONParser();
+        try {
+            String path = "./plugins/MedievalFactions/en-us.json";
+            FileReader reader = new FileReader(path);
+            JSONObject obj = (JSONObject) parser.parse(reader);
+
+            Set<String> keys = obj.keySet();
+
+            for (String key : keys) {
+                strings.put(key, (String) obj.get(key));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error loading from JSON!");
+            e.printStackTrace();
+        }
     }
 
     private void saveToJSON() {
