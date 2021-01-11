cc4d6604d78c5679033ff3ad09386d37c2307606
dmccoystephenson <dmccoystephenson@gmail.com>
2021-01-10 17:17:28 -0700

    altered loadFromJSON file

--- src/main/java/dansplugins/factionsystem/LocaleManager.java
+++ src/main/java/dansplugins/factionsystem/LocaleManager.java
@@ -4,16 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Set;
 
 public class LocaleManager {
 
     private static LocaleManager instance;
+
+    public static final int DEFAULT_BUFFER_SIZE = 8192;
 
     private HashMap<String, String> strings = new HashMap<>();
 
@@ -44,8 +44,10 @@ public class LocaleManager {
         JSONParser parser = new JSONParser();
         try {
             String path = "en-us.json";
-            InputStream stream = MedievalFactions.getInstance().getResource(path);
-            InputStreamReader reader = new InputStreamReader(stream);
+            File file = new File("./plugins/MedievalFactions/" + path);
+            InputStream inputStream = MedievalFactions.getInstance().getResource(path);
+            copyInputStreamToFile(inputStream, file);
+            FileReader reader = new FileReader(file);
             JSONObject obj = (JSONObject) parser.parse(reader);
 
             Set<String> keys = obj.keySet();
@@ -58,6 +60,20 @@ public class LocaleManager {
             System.out.println("Error loading from JSON!");
             e.printStackTrace();
         }
+    }
+
+    // source: https://mkyong.com/java/how-to-convert-inputstream-to-file-in-java/
+    private void copyInputStreamToFile(InputStream inputStream, File file) throws IOException {
+
+        // append = false
+        try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
+            int read;
+            byte[] bytes = new byte[DEFAULT_BUFFER_SIZE];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        }
+
     }
 
     private void saveToJSON() {
