99873ece85bbc33bcc6dd961cddad5d4a98e8021
dmccoystephenson <dmccoystephenson@gmail.com>
2021-01-11 11:46:10 -0700

    made AddLawCommand.java translatable

--- src/main/java/dansplugins/factionsystem/commands/AddLawCommand.java
+++ src/main/java/dansplugins/factionsystem/commands/AddLawCommand.java
@@ -1,5 +1,6 @@
 package dansplugins.factionsystem.commands;
 
+import dansplugins.factionsystem.LocaleManager;
 import dansplugins.factionsystem.data.PersistentData;
 import dansplugins.factionsystem.objects.Faction;
 import dansplugins.factionsystem.utils.ArgumentParser;
@@ -24,24 +25,24 @@ public class AddLawCommand {
 
                         playersFaction.addLaw(newLaw);
 
-                        player.sendMessage(ChatColor.GREEN + "Law added!");
+                        player.sendMessage(ChatColor.GREEN + LocaleManager.getInstance().getText("LawAdded"));
                     }
                     else {
-                        player.sendMessage(ChatColor.RED + "Usage: /mf addlaw (new law)");
+                        player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("UsageAddLaw"));
                     }
 
                 }
                 else {
-                    player.sendMessage(ChatColor.RED + "You need to be the owner of your faction to use this command!");
+                    player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("AlertMustBeOwnerToUseCommand"));
                 }
 
             }
             else {
-                player.sendMessage(ChatColor.RED + "You need to be in a faction to use this command!");
+                player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("AlertMustBeInFactionToUseCommand"));
             }
         }
         else {
-            sender.sendMessage(ChatColor.RED + "Sorry! In order to use this command, you need the following permission: 'mf.addlaw'");
+            sender.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("PermissionAddLaw"));
         }
 
     }
