738df608dede0a10688e060e1466560f99faa518
dmccoystephenson <dmccoystephenson@gmail.com>
2021-01-10 18:53:00 -0700

    implemented loadFromResource() using a .tsv file

--- src/main/java/dansplugins/factionsystem/LocaleManager.java
+++ src/main/java/dansplugins/factionsystem/LocaleManager.java
@@ -1,5 +1,6 @@
 package dansplugins.factionsystem;
 
+import dansplugins.factionsystem.utils.Pair;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -7,6 +8,7 @@ import org.json.simple.parser.JSONParser;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -16,6 +18,7 @@ public class LocaleManager {
 
     public static final int DEFAULT_BUFFER_SIZE = 8192;
 
+    private ArrayList<String> keys = new ArrayList<>();
     private HashMap<String, String> strings = new HashMap<>();
 
     private LocaleManager() {
@@ -30,36 +33,45 @@ public class LocaleManager {
     }
 
     public void loadStrings() {
-        loadFromJSON();
+        if (isFilePresent("./plugins/MedievalFactions/en-us.tsv")) {
+            loadFromPluginFolder();
+        }
+        else {
+            loadFromResource();
+        }
     }
 
-    public void saveStrings() {
-        saveToJSON();
+    private boolean isFilePresent(String path) {
+        File file = new File(path);
+        return file.exists();
+    }
+
+    private void loadFromPluginFolder() {
+        // TODO
     }
 
     public String getText(String key) {
         return strings.get(key);
     }
 
-    private void loadFromJSON() {
-        JSONParser parser = new JSONParser();
+    private void loadFromResource() {
         try {
             // prepare file object
-            String fileName = "en-us.json";
-            File file = new File("./plugins/MedievalFactions/" + fileName);
+            String fileName = "en-us.tsv";
 
             // get resource as input stream
             InputStream inputStream = MedievalFactions.getInstance().getResource(fileName);
 
-            // TODO: turn input stream into JSON object
+            InputStreamReader reader = new InputStreamReader(inputStream);
 
-            // get keys from json object
-//            Set<String> keys = obj.keySet();
+            BufferedReader br = new BufferedReader(reader);
+            br.lines().forEach(line -> {
+                Pair<String, String> pair = getPairFromLine(line);
+                strings.put(pair.getLeft(), pair.getRight());
+                keys.add(pair.getLeft());
+            });
 
-            // for each key
-//            for (String key : keys) {
-//                strings.put(key, (String) obj.get(key)); // place key
-//            }
+            saveToPluginFolder();
 
         } catch (Exception e) {
             System.out.println("Error loading from JSON!");
@@ -67,8 +79,41 @@ public class LocaleManager {
         }
     }
 
-    private void saveToJSON() {
-        // TODO
+    private Pair<String, String> getPairFromLine(String line) {
+        String key = "";
+        String value = "";
+
+        int tabIndex = getIndexOfTab(line);
+        key = line.substring(0, tabIndex);
+        value = line.substring(tabIndex + 1);
+
+        return new Pair<>(key, value);
+    }
+
+    private int getIndexOfTab(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '\t') {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void saveToPluginFolder() {
+        File file = new File("./plugins/MedievalFactions/en-us.tsv");
+        try {
+            file.createNewFile();
+
+            FileWriter writer = new FileWriter(file);
+
+            for (String key : keys) {
+                writer.write(key + "\t" + strings.get(key));
+            }
+
+        } catch (Exception e) {
+            System.out.println("There was a problem saving the strings.");
+            e.printStackTrace();
+        }
     }
 
 }
