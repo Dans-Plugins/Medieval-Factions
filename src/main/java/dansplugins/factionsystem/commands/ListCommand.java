fac19645b2672ff8a4d65ea07a23e6a7e43b0840
dmccoystephenson <dmccoystephenson@gmail.com>
2021-01-11 22:14:23 -0700

    made ListCommand translatable

--- src/main/java/dansplugins/factionsystem/commands/ListCommand.java
+++ src/main/java/dansplugins/factionsystem/commands/ListCommand.java
@@ -1,6 +1,7 @@
 package dansplugins.factionsystem.commands;
 
 import dansplugins.factionsystem.ChunkManager;
+import dansplugins.factionsystem.LocaleManager;
 import dansplugins.factionsystem.data.PersistentData;
 import dansplugins.factionsystem.objects.Faction;
 import org.bukkit.ChatColor;
@@ -15,23 +16,23 @@ public class ListCommand {
         if (sender.hasPermission("mf.list")) {
             // if there aren't any factions
             if (PersistentData.getInstance().getFactions().size() == 0) {
-                sender.sendMessage(ChatColor.AQUA + "There are currently no factions.");
+                sender.sendMessage(ChatColor.AQUA + LocaleManager.getInstance().getText("CurrentlyNoFactions"));
             }
             // factions exist, list them
             else {
-                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + " == Factions" + " == ");
+                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + LocaleManager.getInstance().getText("FactionsTitle"));
                 listFactionsWithFormatting(sender);
             }
             return true;
         }
         else {
-            sender.sendMessage(ChatColor.RED + "Sorry! You need the following permission to use this command: 'mf.list'");
+            sender.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("PermissionList"));
             return false;
         }
     }
 
     private void listFactionsWithFormatting(CommandSender sender) {
-        sender.sendMessage(ChatColor.AQUA + "P: power, M: members, L: land");
+        sender.sendMessage(ChatColor.AQUA + LocaleManager.getInstance().getText("ListLegend"));
         sender.sendMessage(ChatColor.AQUA + "-----");
         for (Faction faction : getFactionsSortedByPower()) {
             sender.sendMessage(ChatColor.AQUA + String.format("%-25s %10s %10s %10s", faction.getName(), "P: " + faction.getCumulativePowerLevel(), "M: " + faction.getPopulation(), "L: " + ChunkManager.getInstance().getChunksClaimedByFaction(faction.getName(), PersistentData.getInstance().getClaimedChunks())));
