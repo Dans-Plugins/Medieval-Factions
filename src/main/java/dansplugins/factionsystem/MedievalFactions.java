c7a302d4df4ee55e1fad429997a988eec16f550f
dmccoystephenson <dmccoystephenson@gmail.com>
2021-01-14 00:44:25 -0700

    incremented version to v4.0-alpha-3

--- src/main/java/dansplugins/factionsystem/MedievalFactions.java
+++ src/main/java/dansplugins/factionsystem/MedievalFactions.java
@@ -16,7 +16,7 @@ public class MedievalFactions extends JavaPlugin {
 
     private static MedievalFactions instance;
 
-    private String version = "v4.0-alpha-2";
+    private String version = "v4.0-alpha-3";
 
     public static MedievalFactions getInstance() {
         return instance;
