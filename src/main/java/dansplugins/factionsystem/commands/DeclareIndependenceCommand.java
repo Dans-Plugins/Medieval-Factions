e3bbc3532f55bd4befa7025de41813c4f267ca88
dmccoystephenson <dmccoystephenson@gmail.com>
2021-01-25 17:51:57 -0700

    rewrite DeclareIndependenceCommand.java

--- src/main/java/dansplugins/factionsystem/commands/DeclareIndependenceCommand.java
+++ src/main/java/dansplugins/factionsystem/commands/DeclareIndependenceCommand.java
@@ -10,56 +10,51 @@ import org.bukkit.entity.Player;
 
 public class DeclareIndependenceCommand {
 
-    public void declareIndependence(CommandSender sender) {
+    public boolean declareIndependence(CommandSender sender) {
 
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-
-            if (player.hasPermission("mf.declareindependence")) {
-
-                Faction playersFaction = PersistentData.getInstance().getPlayersFaction(player.getUniqueId());
-
-                    if (playersFaction != null) {
-                        // if faction has liege
-                        if (playersFaction.hasLiege()) {
-
-                            Faction targetFaction = PersistentData.getInstance().getFaction(playersFaction.getLiege());
-
-                            // if owner of faction
-                            if (playersFaction.isOwner(player.getUniqueId())) {
-
-                                // break vassal agreement
-                                targetFaction.removeVassal(playersFaction.getName());
-                                playersFaction.setLiege("none");
-
-                                // add enemy to declarer's faction's enemyList and the enemyLists of its allies
-                                playersFaction.addEnemy(targetFaction.getName());
-
-                                // add declarer's faction to new enemy's enemyList
-                                targetFaction.addEnemy(playersFaction.getName());
-
-                                Messenger.getInstance().sendAllPlayersOnServerMessage(ChatColor.RED + "" + String.format(LocaleManager.getInstance().getText("HasDeclaredIndependence"), playersFaction.getName(), targetFaction.getName()));
-                           }
-                            else {
-                                // tell player they must be owner
-                                player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("MustBeOwner"));
-                            }
-
-                        }
-                        else {
-                            player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("NotAVassalOfAFaction"));
-                        }
-                    }
-                    else {
-                        player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("AlertMustBeInFactionToUseCommand"));
-                    }
-
-            }
-            else {
-                sender.sendMessage(ChatColor.RED + String.format(LocaleManager.getInstance().getText("PermissionNeeded"), "mf.declareindependence"));
-            }
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(LocaleManager.getInstance().getText("OnlyPlayersCanUseCommand"));
+            return false;
         }
 
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("mf.declareindependence")) {
+            sender.sendMessage(ChatColor.RED + String.format(LocaleManager.getInstance().getText("PermissionNeeded"), "mf.declareindependence"));
+            return false;
+        }
+
+        Faction playersFaction = PersistentData.getInstance().getPlayersFaction(player.getUniqueId());
+
+        if (playersFaction == null) {
+            player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("AlertMustBeInFactionToUseCommand"));
+            return false;
+        }
+
+        if (!playersFaction.isOwner(player.getUniqueId())) {
+            player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("AlertMustBeOwnerToUseCommand"));
+            return false;
+        }
+
+        if (!playersFaction.hasLiege()) {
+            player.sendMessage(ChatColor.RED + LocaleManager.getInstance().getText("NotAVassalOfAFaction"));
+            return false;
+        }
+
+        Faction targetFaction = PersistentData.getInstance().getFaction(playersFaction.getLiege());
+
+        // break vassal agreement
+        targetFaction.removeVassal(playersFaction.getName());
+        playersFaction.setLiege("none");
+
+        // add enemy to declarer's faction's enemyList and the enemyLists of its allies
+        playersFaction.addEnemy(targetFaction.getName());
+
+        // add declarer's faction to new enemy's enemyList
+        targetFaction.addEnemy(playersFaction.getName());
+
+        Messenger.getInstance().sendAllPlayersOnServerMessage(ChatColor.RED + "" + String.format(LocaleManager.getInstance().getText("HasDeclaredIndependence"), playersFaction.getName(), targetFaction.getName()));
+        return true;
     }
 
 }
