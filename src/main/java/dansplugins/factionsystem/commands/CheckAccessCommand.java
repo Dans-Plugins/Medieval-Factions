1eb47a7086b1e3128bc8cdae8a31590be88fe732
Callum Johnson <cjohnson@sky.com>
2021-05-06 16:22:51 +0100

    Updated CheckAccessCommand's implementation.

--- src/main/java/dansplugins/factionsystem/commands/CheckAccessCommand.java
+++ src/main/java/dansplugins/factionsystem/commands/CheckAccessCommand.java
@@ -1,13 +1,58 @@
 package dansplugins.factionsystem.commands;
 
 import dansplugins.factionsystem.LocaleManager;
+import dansplugins.factionsystem.commands.abs.SubCommand;
 import dansplugins.factionsystem.data.EphemeralData;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CheckAccessCommand {
+public class CheckAccessCommand extends SubCommand {
 
+    public CheckAccessCommand() {
+        super(new String[] {
+            ""
+        }, true, false);
+    }
+
+    /**
+     * Method to execute the command for a player.
+     *
+     * @param player who sent the command.
+     * @param args   of the command.
+     * @param key    of the sub-command (e.g. Ally).
+     */
+    @Override
+    public void execute(Player player, String[] args, String key) {
+        final String permission = "mf.checkaccess";
+        if (!(checkPermissions(player, permission))) return;
+        boolean cancel = false, contains = ephemeral.getPlayersCheckingAccess().contains(player.getUniqueId());
+        if (args.length >= 1) cancel = args[0].equalsIgnoreCase("cancel");
+        if (cancel && contains) {
+            ephemeral.getPlayersCheckingAccess().remove(player.getUniqueId());
+            player.sendMessage(translate("&c" + getText("Cancelled")));
+        } else {
+            if (contains) player.sendMessage(translate("&c" + getText("AlreadyEnteredCheckAccess")));
+            else {
+                ephemeral.getPlayersCheckingAccess().add(player.getUniqueId());
+                player.sendMessage(translate("&c" + getText("RightClickCheckAccess")));
+            }
+        }
+    }
+
+    /**
+     * Method to execute the command.
+     *
+     * @param sender who sent the command.
+     * @param args   of the command.
+     * @param key    of the command.
+     */
+    @Override
+    public void execute(CommandSender sender, String[] args, String key) {
+
+    }
+
+    @Deprecated
     public boolean checkAccess(CommandSender sender, String[] args) {
 
         if (!(sender instanceof Player)) {
