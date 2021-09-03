cfeadcfb1b04a418c87d153f8ed1d2a05e2b95ff
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-03 17:16:04 -0600

    Created IFactionInfo, FactionInfo, IMedievalFactionsAPI and MedievalFactionsAPI

--- src/main/java/dansplugins/factionsystem/externalapi/IMedievalFactionsAPI.java
+++ src/main/java/dansplugins/factionsystem/externalapi/IMedievalFactionsAPI.java
@@ -1,6 +1,8 @@
 package dansplugins.factionsystem.externalapi;
 
+import org.bukkit.entity.Player;
+
 public interface IMedievalFactionsAPI {
     FactionInfo getFaction(String factionName);
-
+    FactionInfo getFaction(Player player);
 }
--- src/main/java/dansplugins/factionsystem/externalapi/MedievalFactionsAPI.java
+++ src/main/java/dansplugins/factionsystem/externalapi/MedievalFactionsAPI.java
@@ -1,11 +1,17 @@
 package dansplugins.factionsystem.externalapi;
 
 import dansplugins.factionsystem.data.PersistentData;
+import org.bukkit.entity.Player;
 
 public class MedievalFactionsAPI implements IMedievalFactionsAPI {
 
     @Override
     public FactionInfo getFaction(String factionName) {
         return new FactionInfo(PersistentData.getInstance().getFaction(factionName));
+    }
+
+    @Override
+    public FactionInfo getFaction(Player player) {
+        return new FactionInfo(PersistentData.getInstance().getPlayersFaction(player.getUniqueId()));
     }
 }
