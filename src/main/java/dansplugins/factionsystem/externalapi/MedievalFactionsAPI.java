a0c2511ac36b31c569d75ef24f11a22c34a3b51b
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-18 20:31:36 -0600

    Changed API version to v1.0.0

--- src/main/java/dansplugins/factionsystem/externalapi/MedievalFactionsAPI.java
+++ src/main/java/dansplugins/factionsystem/externalapi/MedievalFactionsAPI.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 */
 public class MedievalFactionsAPI implements IMedievalFactionsAPI {
 
-    private final String APIVersion = "0.0.5"; // every time the external API is altered, this should be incremented
+    private final String APIVersion = "v1.0.0"; // every time the external API is altered, this should be incremented
 
     // accessors
 
