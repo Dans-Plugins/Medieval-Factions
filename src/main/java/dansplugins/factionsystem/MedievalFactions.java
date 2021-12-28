45e0e38abf697ca7a3fcfca85100b478a3d107bf
dmccoystephenson <dmccoystephenson@gmail.com>
2021-12-28 16:01:36 -0700

    Added comments and annotations to the main class.

--- src/main/java/dansplugins/factionsystem/MedievalFactions.java
+++ src/main/java/dansplugins/factionsystem/MedievalFactions.java
@@ -29,10 +29,17 @@ public class MedievalFactions extends AbstractPonderPlugin {
     private static MedievalFactions instance;
     private final String pluginVersion = "v" + getDescription().getVersion();
 
+    /**
+     * This can be used to get the instance of the main class that is managed by itself.
+     * @return The managed instance of the main class.
+     */
     public static MedievalFactions getInstance() {
         return instance;
     }
 
+    /**
+     * This runs when the server starts.
+     */
     @Override
     public void onEnable() {
         instance = this;
@@ -48,19 +55,39 @@ public class MedievalFactions extends AbstractPonderPlugin {
         PersistentData.getInstance().createActivityRecordForEveryOfflinePlayer();
     }
 
+    /**
+     * This runs when the server stops.
+     */
     @Override
     public void onDisable() {
         LocalStorageService.getInstance().save();
     }
 
+    /**
+     * This method handles commands sent to the minecraft server and interprets them if the label matches one of the core commands.
+     * @param sender The sender of the command.
+     * @param cmd The command that was sent. This is unused.
+     * @param label The core command that has been invoked.
+     * @param args Arguments of the core command. Often sub-commands.
+     * @return A boolean indicating whether the execution of the command was successful.
+     */
+    @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         return LocalCommandService.getInstance().interpretCommand(sender, label, args);
     }
 
+    /**
+     * This can be used to get the version of the plugin.
+     * @return A string containing the version preceded by 'v'
+     */
     public String getVersion() {
         return pluginVersion;
     }
 
+    /**
+     * Checks if the version is mismatched.
+     * @return A boolean indicating if the version is mismatched.
+     */
     public boolean isVersionMismatched() {
         String configVersion = getConfig().getString("version");
         if (configVersion == null || this.getVersion() == null) {
@@ -70,14 +97,25 @@ public class MedievalFactions extends AbstractPonderPlugin {
         }
     }
 
+    /**
+     * This can be utilized to access the external API of Medieval Factions.
+     * @return A reference to the external API.
+     */
     public MedievalFactionsAPI getAPI() {
         return new MedievalFactionsAPI();
     }
 
+    /**
+     * Checks if debug is enabled.
+     * @return Whether debug is enabled.
+     */
     public boolean isDebugEnabled() {
         return getConfig().getBoolean("debugMode");
     }
 
+    /**
+     * Creates or loads the config, depending on the situation.
+     */
     private void initializeConfig() {
         if (!(new File("./plugins/MedievalFactions/config.yml").exists())) {
             LocalConfigService.getInstance().saveConfigDefaults();
@@ -91,17 +129,26 @@ public class MedievalFactions extends AbstractPonderPlugin {
         }
     }
 
+    /**
+     * Loads stored data into Persistent Data.
+     */
     private void load() {
         LocalLocaleService.getInstance().loadStrings();
         LocalStorageService.getInstance().load();
     }
 
+    /**
+     * Calls the Scheduler to schedule tasks that have to repeatedly be executed.
+     */
     private void scheduleRecurringTasks() {
         Scheduler.getInstance().schedulePowerIncrease();
         Scheduler.getInstance().schedulePowerDecrease();
         Scheduler.getInstance().scheduleAutosave();
     }
 
+    /**
+     * Registers the event handlers of the plugin using Ponder.
+     */
     private void registerEventHandlers() {
         ArrayList<Listener> listeners = new ArrayList<>(Arrays.asList(
                 new ChatHandler(),
@@ -113,6 +160,9 @@ public class MedievalFactions extends AbstractPonderPlugin {
         getToolbox().getEventHandlerRegistry().registerEventHandlers(listeners, this);
     }
 
+    /**
+     * Takes care of integrations for other plugins and tools.
+     */
     private void handleIntegrations() {
         // bStats
         int pluginId = 8929;
