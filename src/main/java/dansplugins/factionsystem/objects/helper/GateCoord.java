c8615a2180340d73cc9ba3165054a4a08377eca0
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-22 14:33:26 -0700

    Fixed some compile-time errors.

--- src/main/java/dansplugins/factionsystem/objects/helper/GateCoord.java
+++ src/main/java/dansplugins/factionsystem/objects/helper/GateCoord.java
@@ -9,32 +9,19 @@ import org.bukkit.block.Block;
 /**
  * @author Daniel McCoy Stephenson
  */
-public class GateCoord implements dansplugins.factionsystem.objects.helper.specification.IGateCoord {
-	
+public class GateCoord {
 	private int x;
 	private int y;
 	private int z;
 	private String world;
 
-	@Override
 	public String getWorld()
 	{
 		return world;
 	}
-
-	@Override
-	public String toString()
-	{
-		return String.format("%d,%d,%d,%s", x, y, z, world);
-	}
 	
-	public static GateCoord fromString(String data)
-	{
-		String parts[] = data.split(",");
-		for (String part : parts)
-		{
-//			System.out.println(part);
-		}
+	public static GateCoord fromString(String data) {
+		String[] parts = data.split(",");
 
 		GateCoord coord = new GateCoord();
 		coord.x = Integer.parseInt(parts[0]);
@@ -44,50 +31,48 @@ public class GateCoord implements dansplugins.factionsystem.objects.helper.speci
 		return coord;
 	}
 
-	@Override
 	public int getX()
 	{
 		return x;
 	}
 
-	@Override
 	public int getY()
 	{
 		return y;
 	}
 
-	@Override
 	public int getZ()
 	{
 		return z;
 	}
 	
 	// Only use this when instantiating for load from JSON
-	public GateCoord()
-	{
+	public GateCoord() {
 		
 	}
 	
-	public GateCoord(int X, int Y, int Z, String World)
-	{
+	public GateCoord(int X, int Y, int Z, String World) {
 		x = X;
 		y = Y;
 		z = Z;
 		world = World;
 	}
 	
-	public GateCoord(Block block)
-	{
+	public GateCoord(Block block) {
 		x = block.getX();
 		y = block.getY();
 		z = block.getZ();
 		world = block.getWorld().getName();
 	}
 
-	@Override
-	public boolean equals(Block block)
-	{
+	public boolean equals(Block block) {
 		return block.getX() == x && block.getY() == y && block.getZ() == z && block.getWorld().getName().equalsIgnoreCase(world);
+	}
+
+	@Override
+	public String toString()
+	{
+		return String.format("%d,%d,%d,%s", x, y, z, world);
 	}
 		
 }
