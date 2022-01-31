2b9fd7c5a90558f46ef2b89a455400134cafd43d
Mr-Deej <gavinpersonal9@gmail.com>
2022-01-30 21:28:12 -0600

    added a factionless

--- src/main/java/dansplugins/factionsystem/commands/LockCommand.java
+++ src/main/java/dansplugins/factionsystem/commands/LockCommand.java
@@ -5,6 +5,7 @@
 package dansplugins.factionsystem.commands;
 
 import dansplugins.factionsystem.commands.abs.SubCommand;
+import dansplugins.factionsystem.data.PersistentData;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -30,6 +31,7 @@ public class LockCommand extends SubCommand {
     @Override
     public void execute(Player player, String[] args, String key) {
         final String permission = "mf.lock";
+        if (!(playerNotInFaction(player))) return;
         if (!(checkPermissions(player, permission))) return;
         if (args.length >= 1 && safeEquals(args[0], "cancel")) {
             if (ephemeral.getLockingPlayers().remove(player.getUniqueId())) { // Remove them
@@ -52,5 +54,9 @@ public class LockCommand extends SubCommand {
     @Override
     public void execute(CommandSender sender, String[] args, String key) {
 
+    }
+
+    private boolean playerNotInFaction(Player player) {
+        return PersistentData.getInstance().getPlayersFaction(player.getUniqueId()) == null;
     }
 }
