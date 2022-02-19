371aa2717c89ddc29fbc41832649ba948ecb32e8
dmccoystephenson <dmccoystephenson@gmail.com>
2022-02-19 10:21:28 -0700

    Removed dependencies folder.

--- scripts/install-dependencies.sh
+++
@@ -1,5 +0,0 @@
-# install ponder
-mvn install:install-file -Dfile=${PATH_TO_PONDER_JAR} -DgroupId=preponderous -DartifactId=ponder -Dversion=${PONDER_VERSION} -Dpackaging=Jar
-
-# install fiefs
-mvn install:install-file -Dfile=${PATH_TO_FIEFS_JAR} -DgroupId=dansplugins -DartifactId=fiefs -Dversion=${FIEFS_VERSION} -Dpackaging=Jar
