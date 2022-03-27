5052e2f1b70415b7bb60c6024468cf1e5bc3fc51
dmccoystephenson <dmccoystephenson@gmail.com>
2022-03-27 15:57:10 +0000

    Added key usage tracking capabilities to the Locale utility class.

--- src/main/java/dansplugins/factionsystem/utils/Locale.java
+++ src/main/java/dansplugins/factionsystem/utils/Locale.java
@@ -1,13 +1,29 @@
 package dansplugins.factionsystem.utils;
 
+import java.util.HashMap;
+
 import dansplugins.factionsystem.services.LocalLocaleService;
 
 /**
  * @author Daniel McCoy Stephenson
  */
 public class Locale {
+
+    private static HashMap<String, Integer> keyUses = new HashMap<>();
     
     public static String get(String key) {
+        recordKeyUse(key);
         return LocalLocaleService.getInstance().getText(key);
+    }
+
+    static void recordKeyUse(String key) {
+        if (keyUses.containsKey(key)) {
+            int numUses = keyUses.get(key);
+            keyUses.replace(key, numUses + 1);
+        }
+        else {
+            keyUses.put(key, 1);
+        }
+        Logger.getInstance().debug("The '" + key + "' key has been used " + keyUses.get(key) + " times since the server was started.");
     }
 }
