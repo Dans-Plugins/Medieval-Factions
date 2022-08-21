3eb97027056163df8600fbc2a19875bc4e92ffa8
Ren Binden <ren.binden@gmail.com>
2022-08-21 22:39:09 +0100

    Allow players to set member roles, closes #1439

--- src/main/kotlin/com/dansplugins/factionsystem/command/faction/role/MfFactionRoleCommand.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/command/faction/role/MfFactionRoleCommand.kt
@@ -11,6 +11,7 @@ class MfFactionRoleCommand(private val plugin: MedievalFactions) : CommandExecut
     private val factionRoleViewCommand = MfFactionRoleViewCommand(plugin)
     private val factionRoleSetPermissionCommand = MfFactionRoleSetPermissionCommand(plugin)
     private val factionRoleListCommand = MfFactionRoleListCommand(plugin)
+    private val factionRoleSetCommand = MfFactionRoleSetCommand(plugin)
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (args.isEmpty()) {
@@ -21,6 +22,7 @@ class MfFactionRoleCommand(private val plugin: MedievalFactions) : CommandExecut
             "view", plugin.language["CmdFactionRoleView"] -> factionRoleViewCommand.onCommand(sender, command, label, args.drop(1).toTypedArray())
             "setpermission", plugin.language["CmdFactionRoleSetPermission"] -> factionRoleSetPermissionCommand.onCommand(sender, command, label, args.drop(1).toTypedArray())
             "list", plugin.language["CmdFactionRoleList"] -> factionRoleListCommand.onCommand(sender, command, label, args.drop(1).toTypedArray())
+            "set", plugin.language["CmdFactionRoleSet"] -> factionRoleSetCommand.onCommand(sender, command, label, args.drop(1).toTypedArray())
             else -> {
                 sender.sendMessage("$RED${plugin.language["CommandFactionRoleUsage"]}")
                 true
