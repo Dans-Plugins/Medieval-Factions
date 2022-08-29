a5d293c71b0ec145e85ea1d6125470a8fca16975
Ren Binden <ren.binden@gmail.com>
2022-08-29 01:06:48 +0100

    #1440 Implement grant independence command

--- src/main/kotlin/com/dansplugins/factionsystem/command/faction/grantindependence/MfFactionGrantIndependenceCommand.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/command/faction/grantindependence/MfFactionGrantIndependenceCommand.kt
@@ -1,13 +1,84 @@
 package com.dansplugins.factionsystem.command.faction.grantindependence
 
 import com.dansplugins.factionsystem.MedievalFactions
+import com.dansplugins.factionsystem.faction.permission.MfFactionPermission.Companion.GRANT_INDEPENDENCE
+import com.dansplugins.factionsystem.player.MfPlayer
+import com.dansplugins.factionsystem.relationship.MfFactionRelationshipType.LIEGE
+import com.dansplugins.factionsystem.relationship.MfFactionRelationshipType.VASSAL
+import dev.forkhandles.result4k.onFailure
+import org.bukkit.ChatColor.GREEN
+import org.bukkit.ChatColor.RED
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import java.util.logging.Level.SEVERE
 
 class MfFactionGrantIndependenceCommand(private val plugin: MedievalFactions) : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-
+        if (!sender.hasPermission("mf.grantindependence")) {
+            sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceNoPermission"]}")
+            return true
+        }
+        if (args.isEmpty()) {
+            sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceUsage"]}")
+            return true
+        }
+        if (sender !is Player) {
+            sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceNotAPlayer"]}")
+            return true
+        }
+        plugin.server.scheduler.runTaskAsynchronously(plugin, Runnable {
+            val playerService = plugin.services.playerService
+            val mfPlayer = playerService.getPlayer(sender)
+                ?: playerService.save(MfPlayer.fromBukkit(sender)).onFailure {
+                    sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceFailedToSavePlayer"]}")
+                    plugin.logger.log(SEVERE, "Failed to save player: ${it.reason.message}", it.reason.cause)
+                    return@Runnable
+                }
+            val factionService = plugin.services.factionService
+            val faction = factionService.getFaction(mfPlayer.id)
+            if (faction == null) {
+                sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceMustBeInAFaction"]}")
+                return@Runnable
+            }
+            val role = faction.getRole(mfPlayer.id)
+            if (role == null || !role.hasPermission(faction, GRANT_INDEPENDENCE)) {
+                sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceNoFactionPermission"]}")
+                return@Runnable
+            }
+            val target = factionService.getFaction(args.joinToString(" "))
+            if (target == null) {
+                sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceInvalidTarget"]}")
+                return@Runnable
+            }
+            val factionRelationshipService = plugin.services.factionRelationshipService
+            val relationships = factionRelationshipService.getRelationships(faction.id, target.id)
+            val reverseRelationships = factionRelationshipService.getRelationships(target.id, faction.id)
+            if (relationships.none { it.type == VASSAL } || reverseRelationships.none { it.type == LIEGE }) {
+                sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceNotAVassal"]}")
+                return@Runnable
+            }
+            (relationships.filter { it.type == VASSAL } + reverseRelationships.filter { it.type == LIEGE })
+                .forEach { relationship ->
+                    factionRelationshipService.delete(relationship.id).onFailure {
+                        sender.sendMessage("$RED${plugin.language["CommandFactionGrantIndependenceFailedToDeleteRelationship"]}")
+                        plugin.logger.log(SEVERE, "Failed to delete relationship: ${it.reason.message}", it.reason.cause)
+                        return@Runnable
+                    }
+                }
+            sender.sendMessage("$GREEN${plugin.language["CommandFactionGrantIndependenceSuccess", target.name]}")
+            plugin.server.scheduler.runTask(plugin, Runnable {
+                faction.sendMessage(
+                    plugin.language["VassalGrantedIndependenceNotificationTitle", target.name],
+                    plugin.language["VassalGrantedIndependenceNotificationBody", target.name]
+                )
+                target.sendMessage(
+                    plugin.language["FactionGrantedIndependenceNotificationTitle", faction.name],
+                    plugin.language["FactionGrantedIndependenceNotificationBody", faction.name]
+                )
+            })
+        })
         return true
     }
 }
