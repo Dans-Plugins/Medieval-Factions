de307c43ba3e3b3bed064f4ee2207c7f8447a68d
Ren Binden <ren.binden@gmail.com>
2022-10-23 14:34:03 +0100

    Add translation for en-GB

--- src/main/kotlin/com/dansplugins/factionsystem/lang/Language.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/lang/Language.kt
@@ -15,6 +15,7 @@ class Language(plugin: MedievalFactions, private val language: String) {
         if (!languageFolder.exists()) {
             languageFolder.mkdirs()
             plugin.saveResource("lang/lang_en_US.properties", false)
+            plugin.saveResource("lang/lang_en_GB.properties", false)
             plugin.saveResource("lang/lang_de.properties", false)
         }
         val externalUrls = arrayOf(languageFolder.toURI().toURL())
