00bf09d6ba0e8ff9b13da448126c8b40654887b0
Ren Binden <ren.binden@gmail.com>
2022-10-27 20:09:10 +0100

    Add a teleport delay, closes #1559

---
+++ src/main/kotlin/com/dansplugins/factionsystem/listener/PlayerTeleportListener.kt
@@ -0,0 +1,16 @@
+package com.dansplugins.factionsystem.listener
+
+import com.dansplugins.factionsystem.MedievalFactions
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerTeleportEvent
+
+class PlayerTeleportListener(private val plugin: MedievalFactions) : Listener {
+
+    @EventHandler
+    fun onPlayerTeleport(event: PlayerTeleportEvent) {
+        val teleportService = plugin.services.teleportService
+        teleportService.cancelTeleportation(event.player)
+    }
+
+}
