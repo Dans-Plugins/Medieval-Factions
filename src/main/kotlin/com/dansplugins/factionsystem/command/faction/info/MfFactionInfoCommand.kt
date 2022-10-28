a24dbce5955118ba1d35aef4c468c7dfd598c2f9
Ren Binden <ren.binden@gmail.com>
2022-10-28 01:54:51 +0100

    Allow /f info to be used on other players and factions, closes #1560

--- src/main/kotlin/com/dansplugins/factionsystem/command/faction/info/MfFactionInfoCommand.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/command/faction/info/MfFactionInfoCommand.kt
@@ -1,6 +1,7 @@
 package com.dansplugins.factionsystem.command.faction.info
 
 import com.dansplugins.factionsystem.MedievalFactions
+import com.dansplugins.factionsystem.faction.MfFaction
 import com.dansplugins.factionsystem.player.MfPlayer
 import dev.forkhandles.result4k.onFailure
 import net.md_5.bungee.api.chat.ClickEvent
@@ -9,6 +10,7 @@ import net.md_5.bungee.api.chat.HoverEvent
 import net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT
 import net.md_5.bungee.api.chat.TextComponent
 import net.md_5.bungee.api.chat.hover.content.Text
+import org.bukkit.OfflinePlayer
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -24,33 +26,76 @@ class MfFactionInfoCommand(private val plugin: MedievalFactions) : CommandExecut
             sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoNoPermission"]}")
             return true
         }
-        if (sender !is Player) {
+        val hasViewOtherPermission = sender.hasPermission("mf.info.other")
+        if (sender !is Player && !hasViewOtherPermission) {
             sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoNotAPlayer"]}")
             return true
+        }
+        var targetPlayer: OfflinePlayer?
+        if (hasViewOtherPermission && args.isNotEmpty()) {
+            targetPlayer = plugin.server.getOfflinePlayer(args.first())
+            if (!targetPlayer.isOnline && !targetPlayer.hasPlayedBefore()) {
+                targetPlayer = null
+            }
+        } else if (sender is Player) {
+            targetPlayer = sender
+        } else {
+            targetPlayer = null
         }
         plugin.server.scheduler.runTaskAsynchronously(plugin, Runnable {
             // get player's faction
             val playerService = plugin.services.playerService
-            val mfPlayer = playerService.getPlayer(sender)
-                ?: playerService.save(MfPlayer(plugin, sender)).onFailure {
-                    sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoFailedToSavePlayer"]}")
-                    plugin.logger.log(SEVERE, "Failed to save player: ${it.reason.message}", it.reason.cause)
-                    return@Runnable
-                }
+            val senderMfPlayer = if (sender is Player) {
+                playerService.getPlayer(sender)
+                    ?: playerService.save(MfPlayer(plugin, sender)).onFailure {
+                        sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoFailedToSavePlayer"]}")
+                        plugin.logger.log(SEVERE, "Failed to save player: ${it.reason.message}", it.reason.cause)
+                        return@Runnable
+                    }
+            } else {
+                null
+            }
+            val targetMfPlayer = if (targetPlayer != null) {
+                playerService.getPlayer(targetPlayer)
+                    ?: playerService.save(MfPlayer(plugin, targetPlayer)).onFailure {
+                        sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoFailedToSavePlayer"]}")
+                        plugin.logger.log(SEVERE, "Failed to save player: ${it.reason.message}", it.reason.cause)
+                        return@Runnable
+                    }
+            } else {
+                null
+            }
             val factionService = plugin.services.factionService
-            val faction = factionService.getFaction(mfPlayer.id)
+            val faction = when {
+                targetMfPlayer != null -> factionService.getFaction(targetMfPlayer.id)
+                hasViewOtherPermission -> factionService.getFaction(args.joinToString(" "))
+                else -> null
+            }
+            val senderFaction = if (senderMfPlayer != null) {
+                factionService.getFaction(senderMfPlayer.id)
+            } else {
+                null
+            }
             if (faction == null) {
-                sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoMustBeInAFaction"]}")
+                if (args.isEmpty() || !hasViewOtherPermission) {
+                    sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoMustBeInAFaction"]}")
+                } else {
+                    sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoInvalidTarget"]}")
+                }
                 return@Runnable
             }
-            val role = faction.getRole(mfPlayer.id)
-            if (role == null || !role.hasPermission(faction, plugin.factionPermissions.viewInfo)) {
+            val role = if (senderMfPlayer != null) {
+                faction.getRole(senderMfPlayer.id)
+            } else {
+                null
+            }
+            if (!hasViewOtherPermission && (role == null || !role.hasPermission(faction, plugin.factionPermissions.viewInfo))) {
                 sender.sendMessage("${BukkitChatColor.RED}${plugin.language["CommandFactionInfoNoFactionPermission"]}")
                 return@Runnable
             }
             // send player faction info
             sender.sendMessage("${BukkitChatColor.AQUA}${plugin.language["CommandFactionInfoTitle", faction.name]}")
-            if (sender.hasPermission("mf.rename")) {
+            if (sender.hasPermission("mf.rename") && senderFaction?.id == faction.id && role?.hasPermission(faction, plugin.factionPermissions.changeName) == true) {
                 sender.spigot().sendMessage(TextComponent(plugin.language["CommandFactionInfoSetName"]).apply {
                     color = SpigotChatColor.GREEN
                     hoverEvent = HoverEvent(SHOW_TEXT, Text(plugin.language["CommandFactionInfoSetNameHover"]))
@@ -62,7 +107,7 @@ class MfFactionInfoCommand(private val plugin: MedievalFactions) : CommandExecut
             } else {
                 sender.sendMessage("${BukkitChatColor.GRAY}${plugin.language["CommandFactionInfoNoPrefix"]}")
             }
-            if (sender.hasPermission("mf.prefix")) {
+            if (sender.hasPermission("mf.prefix") && senderFaction?.id == faction.id && role?.hasPermission(faction, plugin.factionPermissions.changePrefix) == true) {
                 sender.spigot().sendMessage(TextComponent(plugin.language["CommandFactionInfoSetPrefix"]).apply {
                     color = SpigotChatColor.GREEN
                     hoverEvent = HoverEvent(SHOW_TEXT, Text(plugin.language["CommandFactionInfoSetPrefixHover"]))
@@ -70,7 +115,7 @@ class MfFactionInfoCommand(private val plugin: MedievalFactions) : CommandExecut
                 })
             }
             sender.sendMessage("${BukkitChatColor.GRAY}${plugin.language["CommandFactionInfoDescription", faction.description]}")
-            if (sender.hasPermission("mf.desc")) {
+            if (sender.hasPermission("mf.desc") && senderFaction?.id == faction.id && role?.hasPermission(faction, plugin.factionPermissions.changeDescription) == true) {
                 sender.spigot().sendMessage(TextComponent(plugin.language["CommandFactionInfoSetDescription"]).apply {
                     color = SpigotChatColor.GREEN
                     hoverEvent = HoverEvent(SHOW_TEXT, Text(plugin.language["CommandFactionInfoSetDescriptionHover"]))
@@ -84,7 +129,7 @@ class MfFactionInfoCommand(private val plugin: MedievalFactions) : CommandExecut
             }
             sender.sendMessage("${BukkitChatColor.WHITE}${plugin.language["CommandFactionInfoInvitesTitle", faction.invites.size.toString()]}")
             sender.sendMessage("  ${BukkitChatColor.GRAY}${faction.invites.joinToString { it.playerId.toBukkitPlayer().name ?: plugin.language["UnknownPlayer"] }}")
-            if (sender.hasPermission("mf.invite") && role.hasPermission(faction, plugin.factionPermissions.invite)) {
+            if (sender.hasPermission("mf.invite") && senderFaction?.id == faction.id && role?.hasPermission(faction, plugin.factionPermissions.invite) == true) {
                 sender.spigot().sendMessage(TextComponent(plugin.language["CommandFactionInfoInvite"]).apply {
                     color = SpigotChatColor.GREEN
                     hoverEvent = HoverEvent(SHOW_TEXT, Text(plugin.language["CommandFactionInfoInviteHover"]))
@@ -92,7 +137,7 @@ class MfFactionInfoCommand(private val plugin: MedievalFactions) : CommandExecut
                 })
             }
             factionService.fields
-                .filter { field -> field.isVisibleFor(faction.id.value, mfPlayer.id.value) }
+                .filter { field -> field.isVisibleFor(faction.id.value, senderMfPlayer?.id?.value ?: return@filter false) }
                 .forEach { field ->
                     sender.spigot().sendMessage(
                         TextComponent("${field.name}: ").apply {
@@ -110,5 +155,12 @@ class MfFactionInfoCommand(private val plugin: MedievalFactions) : CommandExecut
         command: Command,
         label: String,
         args: Array<out String>
-    ) = emptyList<String>()
+    ) = when {
+        args.isEmpty() -> plugin.services.factionService.factions.map(MfFaction::name) +
+                plugin.server.offlinePlayers.mapNotNull(OfflinePlayer::getName)
+        args.size == 1 -> (plugin.services.factionService.factions.map(MfFaction::name) +
+                plugin.server.offlinePlayers.mapNotNull(OfflinePlayer::getName))
+            .filter { it.startsWith(args[0]) }
+        else -> emptyList()
+    }
 }
