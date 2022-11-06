ed7defa5ad61476c506194af657bdfcaa963648f
Ren Binden <ren.binden@gmail.com>
2022-11-06 17:59:19 +0000

    Add German translation, closes #1572

--- src/main/kotlin/com/dansplugins/factionsystem/lang/Language.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/lang/Language.kt
@@ -18,6 +18,7 @@ class Language(plugin: MedievalFactions, private val language: String) {
             plugin.saveResource("lang/lang_en_US.properties", false)
             plugin.saveResource("lang/lang_en_GB.properties", false)
             plugin.saveResource("lang/lang_fr_FR.properties", false)
+            plugin.saveResource("lang/lang_de_DE.properties", false)
         }
         val externalUrls = arrayOf(languageFolder.toURI().toURL())
         val externalClassLoader = URLClassLoader(externalUrls)
