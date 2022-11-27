a90d1e75e346c8627ec12af43bf5b210f5c305be
dmccoystephenson <dmccoystephenson@gmail.com>
2022-11-27 14:09:41 -0700

    Made it possible to disband other factions with permission.

--- src/main/kotlin/com/dansplugins/factionsystem/command/faction/disband/MfFactionDisbandCommand.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/command/faction/disband/MfFactionDisbandCommand.kt
@@ -1,6 +1,7 @@
 package com.dansplugins.factionsystem.command.faction.disband
 
 import com.dansplugins.factionsystem.MedievalFactions
+import com.dansplugins.factionsystem.faction.MfFaction
 import com.dansplugins.factionsystem.player.MfPlayer
 import dev.forkhandles.result4k.onFailure
 import org.bukkit.ChatColor.GREEN
@@ -30,21 +31,46 @@ class MfFactionDisbandCommand(private val plugin: MedievalFactions) : CommandExe
                     plugin.logger.log(SEVERE, "Failed to save player: ${it.reason.message}", it.reason.cause)
                     return@Runnable
                 }
+
+            // get faction
             val factionService = plugin.services.factionService
-            val faction = factionService.getFaction(mfPlayer.id)
-            if (faction == null) {
-                sender.sendMessage("$RED${plugin.language["CommandFactionDisbandMustBeInAFaction"]}")
-                return@Runnable
+            val faction: MfFaction?
+            if (args.isEmpty()) {
+                // attempt to use player's faction
+                faction = factionService.getFaction(mfPlayer.id)
+
+                if (faction == null) {
+                    sender.sendMessage("$RED${plugin.language["CommandFactionDisbandMustBeInAFaction"]}")
+                    return@Runnable
+                }
+
+                val role = faction.getRole(mfPlayer.id)
+                if (role == null || !role.hasPermission(faction, plugin.factionPermissions.disband)) {
+                    sender.sendMessage("$RED${plugin.language["CommandFactionDisbandNoFactionPermission"]}")
+                    return@Runnable
+                }
+                if (faction.members.size != 1) {
+                    sender.sendMessage("$RED${plugin.language["CommandFactionDisbandFactionMustBeEmpty"]}")
+                    return@Runnable
+                }
             }
-            val role = faction.getRole(mfPlayer.id)
-            if (role == null || !role.hasPermission(faction, plugin.factionPermissions.disband)) {
-                sender.sendMessage("$RED${plugin.language["CommandFactionDisbandNoFactionPermission"]}")
-                return@Runnable
+            else {
+                // attempt to use specified faction
+                if (!sender.hasPermission("mf.disband.others")) {
+                    sender.sendMessage("$RED${plugin.language["CommandFactionDisbandOthersNoPermission"]}")
+                    return@Runnable
+                }
+
+                val nameOfFaction = args.joinToString(" ");
+                faction = factionService.getFaction(nameOfFaction);
+
+                if (faction == null) {
+                    sender.sendMessage("$RED${plugin.language["CommandFactionDisbandSpecifiedFactionNotFound"]}")
+                    return@Runnable
+                }
             }
-            if (faction.members.size != 1) {
-                sender.sendMessage("$RED${plugin.language["CommandFactionDisbandFactionMustBeEmpty"]}")
-                return@Runnable
-            }
+
+            // delete faction
             factionService.delete(faction.id).onFailure {
                 sender.sendMessage("$RED${plugin.language["CommandFactionDisbandFailedToDeleteFaction"]}")
                 plugin.logger.log(SEVERE, "Failed to delete faction: ${it.reason.message}", it.reason.cause)
