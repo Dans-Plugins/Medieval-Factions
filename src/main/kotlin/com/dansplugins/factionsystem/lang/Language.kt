e0035b15246242d0667c5f23ec95bf60bc38d12a
dmccoystephenson <dmccoystephenson@gmail.com>
2023-01-04 20:46:16 -0700

    Created language files if missing, not just when the language folder is missing.

--- src/main/kotlin/com/dansplugins/factionsystem/lang/Language.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/lang/Language.kt
@@ -15,11 +15,33 @@ class Language(plugin: MedievalFactions, private val language: String) {
         val languageFolder = File(plugin.dataFolder, "lang")
         if (!languageFolder.exists()) {
             languageFolder.mkdirs()
-            plugin.saveResource("lang/lang_en_US.properties", false)
-            plugin.saveResource("lang/lang_en_GB.properties", false)
-            plugin.saveResource("lang/lang_fr_FR.properties", false)
-            plugin.saveResource("lang/lang_de_DE.properties", false)
         }
+
+        val en_us_filename = "lang/lang_en_US.properties"
+        var en_us_file = File(en_us_filename)
+        if (!en_us_file.exists()) {
+            plugin.saveResource(en_us_filename, false)
+        }
+
+        val en_gb_filename = "lang/lang_en_GB.properties"
+        var en_gb_file = File(en_gb_filename)
+        if (!en_gb_file.exists()) {
+            plugin.saveResource(en_gb_filename, false)
+        }
+
+        val fr_fr_filename = "lang/lang_fr_FR.properties"
+        var fr_fr_file = File(fr_fr_filename)
+        if (!fr_fr_file.exists()) {
+            plugin.saveResource(fr_fr_filename, false)
+        }
+
+        val de_de_filename = "lang/lang_de_DE.properties"
+        var de_de_file = File(de_de_filename)
+        if (!de_de_file.exists()) {
+            plugin.saveResource(de_de_filename, false)
+        }
+        plugin.saveResource("lang/lang_de_DE.properties", false)
+
         val externalUrls = arrayOf(languageFolder.toURI().toURL())
         val externalClassLoader = URLClassLoader(externalUrls)
         val externalResourceBundle = ResourceBundle.getBundle(
