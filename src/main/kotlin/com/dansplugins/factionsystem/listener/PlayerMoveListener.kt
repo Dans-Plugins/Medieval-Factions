744ed1618616b425ad6df5256856af2b8abca18a
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2023-01-07 09:38:41 -0700

    Fix - Only Autoclaim With Permission (#1676)
    This fixes players being able to autoclaim even if their role doesn't
    have the 'claim' permission.
    
    closes #1671

--- src/main/kotlin/com/dansplugins/factionsystem/listener/PlayerMoveListener.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/listener/PlayerMoveListener.kt
@@ -38,17 +38,21 @@ class PlayerMoveListener(private val plugin: MedievalFactions) : Listener {
                 val playerFaction = factionService.getFaction(mfPlayer.id)
                 if (playerFaction != null) {
                     if (newChunkFaction == null && playerFaction.autoclaim) {
-                        if (plugin.config.getBoolean("factions.limitLand") && claimService.getClaims(playerFaction.id).size + 1 > playerFaction.power) {
-                            event.player.sendMessage("$RED${plugin.language["AutoclaimPowerLimitReached"]}")
-                            val updatedFaction = factionService.save(playerFaction.copy(autoclaim = false)).onFailure {
-                                plugin.logger.log(SEVERE, "Failed to save faction: ${it.reason.message}", it.reason.cause)
+                        val playerRole = playerFaction.getRole(mfPlayer.id) ?: return@Runnable
+                        val claimPermissionValue = playerRole.getPermissionValue(plugin.factionPermissions.claim) ?: return@Runnable
+                        if (!claimPermissionValue || !event.player.hasPermission("mf.claim")) {
+                            return@Runnableif (plugin.config.getBoolean("factions.limitLand") && claimService.getClaims(playerFaction.id).size + 1 > playerFaction.power) {
+                                event.player.sendMessage("$RED${plugin.language["AutoclaimPowerLimitReached"]}")
+                                val updatedFaction = factionService.save(playerFaction.copy(autoclaim = false)).onFailure {
+                                    plugin.logger.log(SEVERE, "Failed to save faction: ${it.reason.message}", it.reason.cause)
+                                    return@Runnable
+                                }
+                                updatedFaction.sendMessage(
+                                    plugin.language["AutoclaimDisabledNotificationTitle"],
+                                    plugin.language["AutoclaimDisabledNotificationBody"]
+                                )
                                 return@Runnable
                             }
-                            updatedFaction.sendMessage(
-                                plugin.language["AutoclaimDisabledNotificationTitle"],
-                                plugin.language["AutoclaimDisabledNotificationBody"]
-                            )
-                            return@Runnable
                         }
                         if (plugin.config.getBoolean("factions.contiguousClaims") &&
                             !claimService.isClaimAdjacent(playerFaction.id, *listOfNotNull(to.world?.let { MfChunkPosition(it.uid, to.chunk.x, to.chunk.z) }).toTypedArray()) &&
