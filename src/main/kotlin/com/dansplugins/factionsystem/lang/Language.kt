f87bbd4551a716e332ae0e4cb0713ac17577c61b
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2023-05-23 12:04:09 -0600

    Modified Language.kt to include internal resource bundles only if they exist (#1715)
    This should resolve #1712.
    
    Note: ~~these changes are untested as of right now~~ tested!

--- src/main/kotlin/com/dansplugins/factionsystem/lang/Language.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/lang/Language.kt
@@ -9,7 +9,7 @@ import java.util.*
 class Language(plugin: MedievalFactions, private val language: String) {
 
     private val resourceBundles: List<ResourceBundle>
-    val locale = Locale.forLanguageTag(language)
+    val locale: Locale = Locale.forLanguageTag(language)
 
     init {
         val languageFolder = File(plugin.dataFolder, "lang")
@@ -34,11 +34,16 @@ class Language(plugin: MedievalFactions, private val language: String) {
             locale,
             externalClassLoader
         )
-        val internalResourceBundle = ResourceBundle.getBundle(
-            "lang/lang",
-            locale
-        )
-        resourceBundles = listOf(externalResourceBundle, internalResourceBundle)
+
+        resourceBundles = try {
+            val internalResourceBundle = ResourceBundle.getBundle(
+                "lang",
+                locale
+            )
+            listOf(externalResourceBundle, internalResourceBundle)
+        } catch (e: MissingResourceException) {
+            listOf(externalResourceBundle)
+        }
     }
 
     operator fun get(key: String, vararg params: String) =
