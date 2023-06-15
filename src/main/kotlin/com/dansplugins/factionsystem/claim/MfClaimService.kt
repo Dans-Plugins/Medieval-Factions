bacb16b0df3b036b89d428fe47457d8d1420f76d
Donut <86391164+Chocolate1Donut@users.noreply.github.com>
2023-06-15 16:34:37 -0700

    Expanded Territory Title Notifications (#1736)
    This PR expands upon the plugins call to the player.sendTitle method for
    territory notifcations.
    
    - Territory indicator now includes the factions description, displayed
    as a subtitle. This brings it to parity with many other factions
    plugins. By default, it is also configured to be much shorter, as the
    title can become distracting in combat scenarios.
    
    - The length of the title's fadeIn, duration, and fadeOut can now be
    controlled with a config.yml option.
    
    Note that this creates an issue as there is no explicit length limit to
    the factions description, which may cause certain factions descriptions
    to go offscreen when being displayed as a title. Perhaps this might be
    limited, or another string could be stored in a faction, such as a
    tagline?

--- src/main/kotlin/com/dansplugins/factionsystem/claim/MfClaimService.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/claim/MfClaimService.kt
@@ -92,9 +92,12 @@ class MfClaimService(private val plugin: MedievalFactions, private val repositor
                             Runnable {
                                 players.forEach { player ->
                                     val title = "${ChatColor.of(faction.flags[plugin.flags.color])}${faction.name}"
+                                    val subtitle = "${ChatColor.of(faction.flags[plugin.flags.color])}${faction.description}"
                                     if (plugin.config.getBoolean("factions.titleTerritoryIndicator")) {
                                         player.resetTitle()
-                                        player.sendTitle(title, null, 10, 70, 20)
+                                        player.sendTitle(title, subtitle, plugin.config.getInt("factions.titleTerritoryFadeInLength"),
+                                            plugin.config.getInt("factions.titleTerritoryDuration"),
+                                            plugin.config.getInt("factions.titleTerritoryFadeOutLength"))
                                     }
                                     if (plugin.config.getBoolean("factions.actionBarTerritoryIndicator")) {
                                         player.spigot().sendMessage(ACTION_BAR, *TextComponent.fromLegacyText(title))
@@ -138,7 +141,9 @@ class MfClaimService(private val plugin: MedievalFactions, private val repositor
                                 "${ChatColor.of(plugin.config.getString("wilderness.color"))}${plugin.language["Wilderness"]}"
                             if (plugin.config.getBoolean("factions.titleTerritoryIndicator")) {
                                 player.resetTitle()
-                                player.sendTitle(title, null, 10, 70, 20)
+                                player.sendTitle(title, null, plugin.config.getInt("factions.titleTerritoryFadeInLength"),
+                                    plugin.config.getInt("factions.titleTerritoryDuration"),
+                                    plugin.config.getInt("factions.titleTerritoryFadeOutLength"))
                             }
                             if (plugin.config.getBoolean("factions.actionBarTerritoryIndicator")) {
                                 player.spigot().sendMessage(ACTION_BAR, *TextComponent.fromLegacyText(title))
