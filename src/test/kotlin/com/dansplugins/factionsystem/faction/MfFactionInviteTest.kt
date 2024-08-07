7dcdced76c96611d803b1ab16dc200d3fcadc515
dmccoystephenson <dmccoystephenson@gmail.com>
2024-08-06 23:25:49 -0600

    Added unit test for MfFactionInvite class

--- src/test/kotlin/com/dansplugins/factionsystem/faction/MfFactionInviteTest.kt
+++ src/test/kotlin/com/dansplugins/factionsystem/faction/MfFactionInviteTest.kt
@@ -4,18 +4,16 @@ import com.dansplugins.factionsystem.player.MfPlayerId
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class MfFactionApplicationTest {
+class MfFactionInviteTest {
 
     @Test fun testInitialization() {
         // prepare
         val mfPlayerId = MfPlayerId("test")
-        val mfFactionId = MfFactionId.generate()
 
         // execute
-        val mfFactionApplication = MfFactionApplication(mfFactionId, mfPlayerId)
+        val mfFactionInvite = MfFactionInvite(mfPlayerId)
 
         // verify
-        assertEquals(mfPlayerId, mfFactionApplication.applicantId)
-        assertEquals(mfFactionId, mfFactionApplication.factionId)
+        assertEquals(mfPlayerId, mfFactionInvite.playerId)
     }
 }
