228c61fb8f7f0fc0aba86bd5403a1ff426b941a6
dmccoystephenson <dmccoystephenson@gmail.com>
2025-02-25 20:32:55 -0700

    Fixed null pointer error in MfFactionPermissions.kt

--- src/main/kotlin/com/dansplugins/factionsystem/faction/permission/MfFactionPermissions.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/faction/permission/MfFactionPermissions.kt
@@ -68,6 +68,7 @@ class MfFactionPermissions(private val plugin: MedievalFactions) {
                 wrapSimplePermission("CREATE_ROLE", plugin.language["FactionPermissionCreateRole"], false),
                 wrapSimplePermission("SET_DEFAULT_ROLE", plugin.language["FactionPermissionSetDefaultRole"], false),
                 wrapSimplePermission("APPROVE_APP", plugin.language["FactionPermissionApproveApp"], false),
+                wrapSimplePermission("DENY_APP", plugin.language["FactionPermissionDenyApp"], false),
                 SetRolePermission(plugin)
             )
         )
@@ -118,7 +119,7 @@ class MfFactionPermissions(private val plugin: MedievalFactions) {
     val listMembers = parse("LIST_MEMBERS")!!
     val createRole = parse("CREATE_ROLE")!!
     val setDefaultRole = parse("SET_DEFAULT_ROLE")!!
-    val approveApp = parse("APPROVE_APP")!! // TODO: fix null exception
+    val approveApp = parse("APPROVE_APP")!!
     val denyApp = parse("DENY_APP")!!
     fun setRolePermission(permission: MfFactionPermission) = parse("SET_ROLE_PERMISSION(${permission.name})")!!
 
