96ee6ee8a4bb70f1a21c7278918e1306be15a6a5
dmccoystephenson <dmccoystephenson@gmail.com>
2025-03-02 21:05:16 -0700

    Updated ClaimPathBuilderTest to include test case with complex claims

--- src/test/kotlin/com/dansplugins/factionsystem/dynmap/builders/ClaimPathBuilderTest.kt
+++ src/test/kotlin/com/dansplugins/factionsystem/dynmap/builders/ClaimPathBuilderTest.kt
@@ -127,4 +127,30 @@ class ClaimPathBuilderTest {
         )
         assertEquals(expectedPaths, paths)
     }
+
+    @Test
+    fun testGetPaths_complexClaims() {
+        val uuid = UUID.randomUUID()
+        val factionId = MfFactionId("factionId")
+        val claims = listOf(
+            MfClaimedChunk(uuid, 0, 0, factionId),
+            MfClaimedChunk(uuid, 1, 0, factionId),
+            MfClaimedChunk(uuid, 2, 0, factionId),
+            MfClaimedChunk(uuid, 2, 1, factionId),
+            MfClaimedChunk(uuid, 2, 2, factionId),
+            MfClaimedChunk(uuid, 1, 2, factionId),
+            MfClaimedChunk(uuid, 0, 2, factionId),
+            MfClaimedChunk(uuid, 0, 1, factionId),
+            MfClaimedChunk(uuid, 3, 0, factionId),
+            MfClaimedChunk(uuid, 3, 1, factionId),
+            MfClaimedChunk(uuid, 3, 2, factionId),
+            MfClaimedChunk(uuid, 4, 2, factionId)
+        )
+        val paths = ClaimPathBuilder.getPaths(claims)
+        val expectedPaths = listOf(
+            listOf(Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0), Point(4, 0), Point(4, 1), Point(4, 2), Point(5, 2), Point(5, 3), Point(4, 3), Point(3, 3), Point(2, 3), Point(1, 3), Point(0, 3), Point(0, 2), Point(0, 1)),
+            listOf(Point(1, 1), Point(1, 2), Point(2, 2), Point(2, 1))
+        )
+        assertEquals(expectedPaths, paths)
+    }
 }
