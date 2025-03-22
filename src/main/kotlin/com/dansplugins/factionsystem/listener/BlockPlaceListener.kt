60f25710061d44e7a1b7b97e202c0e502a04c10f
dmccoystephenson <dmccoystephenson@gmail.com>
2025-03-21 18:47:25 -0600

    Updated BlockPlaceListener to prevent placing blocks in wilderness if config option enabled

--- src/main/kotlin/com/dansplugins/factionsystem/listener/BlockPlaceListener.kt
+++ src/main/kotlin/com/dansplugins/factionsystem/listener/BlockPlaceListener.kt
@@ -25,7 +25,14 @@ class BlockPlaceListener(private val plugin: MedievalFactions) : Listener {
         }
 
         val claimService = plugin.services.claimService
-        val claim = claimService.getClaim(event.block.chunk) ?: return
+        val claim = claimService.getClaim(event.block.chunk)
+        if (claim == null) {
+            if (plugin.config.getBoolean("wilderness.preventBlockPlace", false)) {
+                event.isCancelled = true
+                event.player.sendMessage("$RED${plugin.language["CannotPlaceBlockInWilderness"]}")
+            }
+            return
+        }
         val factionService = plugin.services.factionService
         val claimFaction = factionService.getFaction(claim.factionId) ?: return
         val relationshipService = plugin.services.factionRelationshipService
